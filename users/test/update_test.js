31a74eae9def6a1fa4b4ee687d2544b58098b1e3
mohdzaid <mohdzaid@gmail.com>
2023-03-12 03:31:25 +0530

    added updae

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -39,5 +39,15 @@ describe("Updating records", () => {
 
   it("A model class can find a record withan Id and update", (done) => {
     User.findByIdAndUpdate(joe._id, { name: "Alex" }, done);
+  });
+
+  it("A user can have their post count increment by 1", (done) => {
+    // assert()
+    User.update({ name: "Joe" }, { $inc: { postCount: 10 } }).then(() =>
+      User.findOne({ name: "Joe" }).then((user) => {
+        assert(user.postCount === 10);
+        done();
+      })
+    );
   });
 });
