b3521ec516642de1982fb0c93961a2aaa75df74d
mohdzaid <mohdzaid@gmail.com>
2023-03-11 17:57:16 +0530

    SL added

--- users/test/delete_test.js
+++ users/test/delete_test.js
@@ -19,9 +19,31 @@ describe("Deleting a user", () => {
       });
   });
 
-  it("class method removed", () => {});
+  it("class method removed", (done) => {
+    // Removed  a bunch of records with some given criteria
+    User.remove({ name: "Joe" })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
 
-  it("class method findAndRemove", () => {});
+  it("class method findOneAndRemove", (done) => {
+    User.findOneAndRemove({ name: "Joe" })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
 
-  it("class method findByIdAndRemove", () => {});
+  it("class method findByIdAndRemove", (done) => {
+    User.findByIdAndRemove(joe._id)
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
 });
---
+++ users/test/update_test.js
@@ -0,0 +1,43 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Updating records", () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+
+  function assertName(operation, done) {
+    operation.then(() => {
+      User.find({}).then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === "Alex");
+        done();
+      });
+    });
+  }
+
+  it("instance type using set n save", (done) => {
+    joe.set("name", "Alex");
+    joe.save();
+    assertName(joe.save(), done);
+  });
+
+  it("A model instance can update", (done) => {
+    assertName(joe.update({ name: "Alex" }), done);
+  });
+
+  it("A model class can update", (done) => {
+    assertName(User.update({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("A model class update one record", (done) => {
+    assertName(User.findOneAndUpdate({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
+  it("A model class can find a record withan Id and update", (done) => {
+    User.findByIdAndUpdate(joe._id, { name: "Alex" }, done);
+  });
+});
