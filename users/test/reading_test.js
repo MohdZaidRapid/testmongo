d223118f3494f2253fefd330b81c1fce5e9425cf
mohdzaid <mohdzaid@gmail.com>
2023-03-14 00:35:28 +0530

    SL added

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -4,8 +4,14 @@ const User = require("../src/user");
 describe("Reading users out of the database", () => {
   let joe;
   beforeEach((done) => {
+    alex = new User({ name: "Alex" });
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+    maria = new User({ name: "Maria" });
+    zack = new User({ name: "Zack" });
+
+    Promise.all([alex.save(), joe.save(), zack.save(), maria.save()]).then(() =>
+      done()
+    );
   });
   it("find all users with a name of joe", (done) => {
     User.find({ name: "Joe" }).then((users) => {
@@ -19,5 +25,18 @@ describe("Reading users out of the database", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip the limit the result set", (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
